4d084b15340841fd5cc63e839d6397539f95e3d6
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-03-30 13:40:04 +0100

    product upload

---
+++ lasagna/src/app/view/ui/form/upload.component.ts
@@ -0,0 +1,36 @@
+import {Component, input, output} from '@angular/core';
+
+@Component({
+  selector: 'lg-upload',
+  standalone: true,
+  template: `
+      <div (click)="input.click()" class="lg-upload">
+          <input #input
+                 (change)="onFileChange($event)"
+                 [accept]="accept()"
+                 type="file">
+          <div class="lg-upload__content">
+              <ng-content></ng-content>
+          </div>
+      </div>
+  `,
+  styles: [
+    `
+      input {
+        display: none;
+      }
+    `
+  ]
+})
+export class UploadComponent {
+  filesSelected = output<File[]>();
+  accept = input<string>('.csv');
+
+  onFileChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file = target.files?.[0];
+    if (file) {
+      this.filesSelected.emit([file]);
+    }
+  }
+}
