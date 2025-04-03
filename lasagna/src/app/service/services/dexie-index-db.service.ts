b4d75825eca2d8e8937339cd2d16a383eacf498c
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-03 19:55:05 +0100

    build

--- lasagna/src/app/service/services/dexie-index-db.service.ts
+++ lasagna/src/app/service/services/dexie-index-db.service.ts
@@ -58,15 +58,15 @@ export class DexieIndexDbService extends Dexie {
     await (this[storeKey] as Table<any>).delete(uuid);
   }
 
-  private generateUuid(): string {
+  generateUuid(): string {
     return Math.random().toString(36).substring(2) + Date.now().toString(36);
   }
 
-  async balkAdd(storeKey: Stores, values: any[]): Promise<void> {
+  async balkAdd(storeKey: Stores, values: any[], autoUUID = true): Promise<void> {
     // @ts-ignore
     await (this[storeKey] as Table<any>).bulkPut(values.map(value => ({
       ...value,
-      uuid: this.generateUuid(),
+      uuid: autoUUID ? this.generateUuid() : value.uuid,
     })));
   }
 }
