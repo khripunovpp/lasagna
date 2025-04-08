65ca5e8e8e7df2bde2ff3b9d51ef661b3f148889
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-08 17:36:45 +0100

    fix pwa button display

--- lasagna/src/app/app.component.ts
+++ lasagna/src/app/app.component.ts
@@ -4,10 +4,11 @@ import {GapColumnComponent} from './view/ui/layout/gap-column.component';
 import {ContainerComponent} from './view/ui/layout/container/container.component';
 import {ButtonComponent} from './view/ui/layout/button.component';
 import {GapRowComponent} from './view/ui/layout/gap-row.component';
+import {PwaInstallComponent} from './view/application/pwa-install.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GapColumnComponent, ContainerComponent, ButtonComponent, GapRowComponent],
+  imports: [RouterOutlet, GapColumnComponent, ContainerComponent, ButtonComponent, GapRowComponent, PwaInstallComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
