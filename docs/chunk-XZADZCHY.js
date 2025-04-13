4368d58d04fd3e3ac206745f0c85516efd6c72f5
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-13 19:16:10 +0100

    build

--- docs/chunk-XZADZCHY.js
+++
@@ -1,1 +0,0 @@
-import{ra as r,za as n}from"./chunk-2LMMMB25.js";var o=class t{constructor(){}width="100%";static \u0275fac=function(i){return new(i||t)};static \u0275dir=r({type:t,selectors:[["","lgExpand",""]],hostVars:2,hostBindings:function(i,e){i&2&&n("width",e.width)}})};export{o as a};
