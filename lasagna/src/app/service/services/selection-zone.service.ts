dbe18e17e603a84cbc36252203793ca23d4950b4
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-15 16:45:08 +0100

    moved selection tools

---
+++ lasagna/src/app/service/services/selection-zone.service.ts
@@ -0,0 +1,42 @@
+import {Injectable, signal} from '@angular/core';
+
+@Injectable()
+export class SelectionZoneService {
+
+  constructor() {
+  }
+
+  selectionMode = signal<'default' | 'selection'>('default');
+  selectAll = signal<boolean>(false);
+  deselectAll = signal<boolean>(false);
+  selected = signal<Set<string>>(new Set());
+
+  onSelection() {
+    this.selectionMode.set(this.selectionMode() === 'default' ? 'selection' : 'default');
+    this.selected.set(new Set());
+  }
+
+  onAllSelection() {
+    this.selectAll.set(true);
+    this.deselectAll.set(false);
+  }
+
+  onDeselectAll() {
+    this.selectAll.set(false);
+    this.deselectAll.set(true);
+  }
+
+  putSelected(selected: [boolean, string]) {
+    const [checked, uuid] = selected;
+    this.selected.update(value => {
+      if (checked) {
+        value.add(uuid);
+      } else {
+        value.delete(uuid);
+      }
+      return value;
+    })
+
+    console.log('selected', this.selected());
+  }
+}
