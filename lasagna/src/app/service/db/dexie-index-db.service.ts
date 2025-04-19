e33dbbd443b11d6148692ffbbbb8965950ea8def
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-19 17:48:27 +0100

    global search

--- lasagna/src/app/service/db/dexie-index-db.service.ts
+++ lasagna/src/app/service/db/dexie-index-db.service.ts
@@ -3,12 +3,15 @@ import Dexie, {Table} from 'dexie';
 import {Stores} from '../const/stores';
 import {migrations} from './migrations';
 import {generateUuid} from '../../helpers/attribute.helper';
+import {FlexsearchIndexService} from './flexsearch-index.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DexieIndexDbService extends Dexie {
-  constructor() {
+  constructor(
+    private flexsearchIndexService: FlexsearchIndexService
+  ) {
     super('lasagna-db');
 
     const seen = new Set();
@@ -23,22 +26,105 @@ export class DexieIndexDbService extends Dexie {
       }
       console.log(`Migration ${migration.version} applied`);
     }
+    setTimeout(() => {
+      this.initIndexes();
+    });
   }
 
   productsStore!: Table<any, string>;
   recipesStore!: Table<any, string>;
   categoryStore!: Table<any, string>;
 
-  async addData(storeKey: Stores, value: any,customUUID?:string): Promise<string> {
-    const uuid = customUUID || this.generateUuid();
-    // @ts-ignore
-    await (this[storeKey] as Table<any>).put({...value, uuid});
-    return uuid;
+  initIndexes(): Promise<void> {
+    return Promise.all([
+      this.initIndex(Stores.PRODUCTS),
+      this.initIndex(Stores.RECIPES),
+      this.initIndex(Stores.PRODUCTS_CATEGORIES),
+      this.initIndex(Stores.RECIPES_CATEGORIES),
+    ]).then(() => {
+
+    });
+  }
+
+  async initIndex(table: string): Promise<void> {
+    await this.flexsearchIndexService.initIndex(this.getStore(table as Stores), table, ['name']);
+    const indexData = await this.getOne(Stores.INDICES, table);
+
+    if (indexData) {
+      console.log('Index loaded:', table, indexData);
+      await this.flexsearchIndexService.importIndex(table, indexData.indexData);
+    } else {
+      console.log('No index found for table:', table);
+      await this.getAll(table as Stores).then(async (data) => {
+        for (const item of data) {
+          await this.flexsearchIndexService.addToIndex(table, item);
+        }
+        console.log('Index created for table:', table);
+      });
+
+      await this.saveIndex(table);
+    }
+
+    // await this.saveIndex(table);
+  }
+
+  async saveIndex(table: string) {
+    try {
+      const indexData = this.flexsearchIndexService.getIndex(table);
+      if (!indexData) {
+        console.error('No index data found for table:', table);
+        return;
+      } else {
+        const data = await this.flexsearchIndexService.exportIndex(table);
+        await this.replaceData(Stores.INDICES, table, {
+          table,
+          indexData: data,
+        });
+      }
+    } catch (error) {
+      console.error('Error saving index:', error);
+      throw error;
+    }
+  }
+
+
+  async loadIndex(table: string) {
+    const indexData = await this.filter(Stores.INDICES, 'table', table, true);
+
+    console.log('Index data:', indexData);
+
+    if (indexData?.[0]) {
+      return indexData[0].indexData;
+    }
+    return null;
+  }
+
+  async addData(storeKey: Stores, value: any, customUUID?: string): Promise<string> {
+    try {
+      const uuid = customUUID || this.generateUuid();
+      const obj = {...value, uuid};
+      // @ts-ignore
+      await (this[storeKey] as Table<any>).put(obj);
+      if (storeKey === Stores.INDICES) {
+        return uuid;
+      }
+      await this.flexsearchIndexService.addToIndex(storeKey, obj);
+      await this.saveIndex(storeKey);
+      return uuid;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async replaceData(storeKey: Stores, uuid: string, value: any): Promise<void> {
+    const obj = {...value, uuid};
     // @ts-ignore
-    await (this[storeKey] as Table<any>).put({...value, uuid});
+    await (this[storeKey] as Table<any>).put(obj);
+    if (storeKey === Stores.INDICES) {
+      return;
+    }
+    await this.flexsearchIndexService.addToIndex(storeKey, obj);
+    await this.saveIndex(storeKey);
   }
 
   async search(storeKey: Stores, indexField: string, value: string): Promise<any[]> {
@@ -46,9 +132,19 @@ export class DexieIndexDbService extends Dexie {
     return (this[storeKey] as Table<any>).where(indexField).equals(value).toArray();
   }
 
-  async filter(storeKey: Stores, indexField: string, value: string): Promise<any[]> {
+  async filter(storeKey: Stores, indexField: string, value: string, exactMatch = false): Promise<any[]> {
     // @ts-ignore
-    return (this[storeKey] as Table<any>).filter((item: any) => item[indexField]?.toLowerCase().includes(value.toLowerCase())).toArray();
+    return (this[storeKey] as Table<any>).filter((item: any) => {
+      if (exactMatch) {
+        return item[indexField] === value;
+      }
+      return item[indexField]?.toLowerCase().includes(value.toLowerCase())
+    }).toArray();
+  }
+
+  getStore(storeKey: Stores): Table<any, string> {
+    // @ts-ignore
+    return this[storeKey] as Table<any, string>;
   }
 
   async getOne(storeKey: Stores, uuid: string): Promise<any> {
@@ -71,6 +167,18 @@ export class DexieIndexDbService extends Dexie {
   async remove(storeKey: Stores, uuid: string): Promise<void> {
     // @ts-ignore
     await (this[storeKey] as Table<any>).delete(uuid);
+    if (storeKey === Stores.INDICES) {
+      return;
+    }
+    await this.flexsearchIndexService.removeFromIndex(storeKey, uuid);
+    await this.saveIndex(storeKey);
+  }
+
+  async clear(storeKey: Stores): Promise<void> {
+    // @ts-ignore
+    await (this[storeKey] as Table<any>).clear();
+    await this.flexsearchIndexService.clearIndex(storeKey);
+    await this.saveIndex(storeKey);
   }
 
   generateUuid(): string {
@@ -83,6 +191,10 @@ export class DexieIndexDbService extends Dexie {
       ...value,
       uuid: autoUUID ? this.generateUuid() : value.uuid,
     })));
+    //
+    // values.forEach(value => {
+    //   this.flexsearchIndexService.add(value.uuid, value);
+    // });
   }
 
   async uniqueKeys(storeKey: Stores, indexField: string): Promise<any[]> {
