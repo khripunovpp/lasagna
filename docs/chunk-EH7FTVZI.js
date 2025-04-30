4cee1c8ab4364223048fa303647a033882ac2e43
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-30 19:39:05 +0100

    build

---
+++ docs/chunk-EH7FTVZI.js
@@ -0,0 +1,1 @@
+import{cb as e,ob as r}from"./chunk-MV5CNZ6J.js";var o=class t{constructor(){}whiteSpace="nowrap";static \u0275fac=function(i){return new(i||t)};static \u0275dir=e({type:t,selectors:[["","lgNoWrap",""]],hostVars:2,hostBindings:function(i,c){i&2&&r("white-space",c.whiteSpace)}})};export{o as a};
