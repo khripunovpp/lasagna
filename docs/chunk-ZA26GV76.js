0bc388ee0c947cb990989023a60aff2276955005
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-04-30 21:59:18 +0100

    build

---
+++ docs/chunk-ZA26GV76.js
@@ -0,0 +1,1 @@
+import{cb as r,ob as e}from"./chunk-RDTQHNEQ.js";var n=class t{constructor(){}marginLeft="auto";static \u0275fac=function(i){return new(i||t)};static \u0275dir=r({type:t,selectors:[["","lgPull",""]],hostVars:2,hostBindings:function(i,s){i&2&&e("margin-left",s.marginLeft)}})};export{n as a};
