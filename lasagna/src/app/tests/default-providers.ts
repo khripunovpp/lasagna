74a08208c3318fa6de624bbeadf0025ae410e705
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-05-08 19:38:06 +0100

    product ui tests

--- lasagna/src/app/tests/default-providers.ts
+++ lasagna/src/app/tests/default-providers.ts
@@ -1,9 +1,11 @@
 import {DexieIndexDbService} from '../service/db/dexie-index-db.service';
 import {DB_NAME} from '../service/tokens/db-name.token';
 import {NotificationsService} from '../service/services';
+import {provideNoopAnimations} from '@angular/platform-browser/animations';
 
 export const defaultProviders = [
   DexieIndexDbService,
   NotificationsService,
   {provide: DB_NAME, useValue: 'TestDB'},
+  provideNoopAnimations(),
 ]
