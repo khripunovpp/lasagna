405a8449563a148cf313d3c204451b8bdf8c7083
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-05-13 09:53:24 +0100

    fix service worker

---
+++ docs/chunk-R6CUI7LZ.js
@@ -0,0 +1,1 @@
+import{fb as t,rb as n}from"./chunk-FSGSBBFI.js";var r=class e{constructor(){}selfAlign="center";static \u0275fac=function(i){return new(i||e)};static \u0275dir=t({type:e,selectors:[["","lgSelfCenter",""]],hostVars:2,hostBindings:function(i,o){i&2&&n("align-self",o.selfAlign)}})};export{r as a};
