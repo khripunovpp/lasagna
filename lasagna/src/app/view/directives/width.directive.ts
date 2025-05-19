3537d97bb3bbd44247be090844744c86c9bff610
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-05-19 10:31:28 +0100

    hotfix mobile

--- lasagna/src/app/view/directives/width.directive.ts
+++ lasagna/src/app/view/directives/width.directive.ts
@@ -1,4 +1,5 @@
 import {Directive, HostBinding, Input} from '@angular/core';
+import {mediaMobMax} from '@helpers/match-media.helper';
 
 @Directive({
   standalone: true,
@@ -11,10 +12,23 @@ export class WidthDirective {
   @Input() lgWidth: number | string = 0;
 
   @HostBinding('style.max-width') get maxWidth() {
+    if (mediaMobMax()) {
+      return '100%';
+    }
     return this.lgWidth;
   }
 
   @HostBinding('style.flex') get flex() {
+    if (mediaMobMax()) {
+      return '1 0 100%';
+    }
     return `1 0 ${this.lgWidth}`;
+  };
+
+  @HostBinding('style.align-self') get alignSelf() {
+    if (mediaMobMax()) {
+      return 'stretch';
+    }
+    return null
   };
 }
