ab6c9e6702ed6657bbf4b3058465fc48d123e3c8
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-05-21 17:28:54 +0100

    some minor fixes

--- lasagna/src/app/view/settings/settings.component.ts
+++ lasagna/src/app/view/settings/settings.component.ts
@@ -14,6 +14,7 @@ import {BackupSettingsComponent} from '@view/settings/backup/backup-settings.com
 import {TranslatePipe} from '@ngx-translate/core';
 import {TabDirective} from '@view/ui/tabs/tab.directive';
 import {TabsComponent} from '@view/ui/tabs/tabs.component';
+import {InlineSeparatedGroupComponent, InlineSeparatedGroupDirective} from '@view/ui/inline-separated-group.component';
 
 @Component({
   selector: 'lg-settings',
@@ -32,7 +33,9 @@ import {TabsComponent} from '@view/ui/tabs/tabs.component';
     BackupSettingsComponent,
     TranslatePipe,
     TabDirective,
-    TabsComponent
+    TabsComponent,
+    InlineSeparatedGroupComponent,
+    InlineSeparatedGroupDirective
   ]
 })
 export class SettingsComponent {
