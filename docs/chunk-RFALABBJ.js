65f04177fbbddac142665d30c76ff3128c63be48
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-05-24 16:11:05 +0100

    build

--- docs/chunk-RFALABBJ.js
+++
@@ -1,1 +0,0 @@
-import{Z as e}from"./chunk-ESI6QU3H.js";var o=new e("CategorizedRecipesList");export{o as a};
---
+++ docs/chunk-ZZ7RMZQH.js
@@ -0,0 +1,1 @@
+import{Z as e}from"./chunk-ESI6QU3H.js";var r=new e("CategorizedRecipesList");export{r as a};
