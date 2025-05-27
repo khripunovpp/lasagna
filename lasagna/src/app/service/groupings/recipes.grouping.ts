f85312865a8a58e6c98f60cdb3f8c63f3275dce1
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-05-27 23:10:37 +0100

    some styles

--- lasagna/src/app/service/groupings/recipes.grouping.ts
+++ lasagna/src/app/service/groupings/recipes.grouping.ts
@@ -14,19 +14,23 @@ export class CategoryRecipeSortStrategy
     direction: 'asc' | 'desc',
     field: string = 'name'
   ): number {
-    if (!a || !b) return 0;
-    if (field === 'name') {
-      if (direction === 'asc') {
-        return (a as any)[field]?.toString()?.localeCompare((b as any)[field]) || 0;
-      } else {
-        return (b as any)[field]?.toString()?.localeCompare((a as any)[field]) || 0;
-      }
-    } else if (field === 'createdAt') {
-      const dateA = new Date((a as any)[field]);
-      const dateB = new Date((b as any)[field]);
-      return direction === 'asc' ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
-    }
-    return 0
+    return recipeSortFunction(a, b, direction, field);
+  }
+}
+
+export class RecipeAlphabeticalSortStrategy
+  extends BaseGrouping<RecipeDTO> {
+  override groupBy(item: RecipeDTO): string {
+    return item.name.toLowerCase().charAt(0);
+  }
+
+  override sort(
+    a: RecipeDTO,
+    b: RecipeDTO,
+    direction: 'asc' | 'desc',
+    field: string = 'name'
+  ): number {
+    return recipeSortFunction(a, b, direction, field);
   }
 }
 
@@ -50,18 +54,27 @@ export class RecipeCreatedAtMonthSortStrategy
     direction: 'asc' | 'desc',
     field: string = 'createdAt'
   ): number {
-    if (!a || !b) return 0;
-    if (field === 'name') {
-      if (direction === 'asc') {
-        return (a as any)[field]?.toString()?.localeCompare((b as any)[field]) || 0;
-      } else {
-        return (b as any)[field]?.toString()?.localeCompare((a as any)[field]) || 0;
-      }
-    } else if (field === 'createdAt') {
-      const dateA = new Date((a as any)[field]);
-      const dateB = new Date((b as any)[field]);
-      return direction === 'asc' ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
-    }
-    return 0
+    return recipeSortFunction(a, b, direction, field);
   }
+}
+
+export const recipeSortFunction = (
+  a: RecipeDTO,
+  b: RecipeDTO,
+  direction: 'asc' | 'desc' = 'asc',
+  field: string = 'name'
+): number => {
+  if (!a || !b) return 0;
+  if (field === 'name') {
+    if (direction === 'asc') {
+      return (a as any)[field]?.toString()?.localeCompare((b as any)[field]) || 0;
+    } else {
+      return (b as any)[field]?.toString()?.localeCompare((a as any)[field]) || 0;
+    }
+  } else if (field === 'createdAt') {
+    const dateA = new Date((a as any)[field]);
+    const dateB = new Date((b as any)[field]);
+    return direction === 'asc' ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
+  }
+  return 0
 }
