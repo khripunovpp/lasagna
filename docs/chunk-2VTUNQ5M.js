e7640e2edd9d92b3baf55c5d6ee4846fc251acce
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-07-07 20:16:26 +0100

    build

---
+++ docs/chunk-2VTUNQ5M.js
@@ -0,0 +1,1 @@
+var e={draft:"text-secondary",paid:"text-success",cancelled:"text-danger",issued:"text-info"},t={draft:"Draft",paid:"Paid",cancelled:"Cancelled",issued:"Issued"};export{e as a,t as b};
