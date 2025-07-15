d726fbcf4dc1ed71ff8120e144d0ae64842cdb18
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-07-15 20:18:27 +0100

    move global search

--- lasagna/src/app/shared/service/services/index.ts
+++ lasagna/src/app/shared/service/services/index.ts
@@ -4,7 +4,7 @@ export * from './csv-reader.service';
 export * from './docs.service';
 export * from './draft-forms.service';
 export * from './form-templates.service';
-export * from './global-search.service';
+export * from '../../../features/global-search/global-search.service';
 export * from './index-db-select-loader.service';
 export * from './localstorage-select-loader.service';
 export * from './notifications.service';
