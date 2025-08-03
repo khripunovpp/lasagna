77ffc117adbc0a4efafd471653e4f69946832e74
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-08-03 17:00:51 +0100

    informers fixes

--- lasagna/src/app/shared/view/ui/pwa-install.component.ts
+++ lasagna/src/app/shared/view/ui/pwa-install.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'lg-pwa-install',
   standalone: true,
   template: `
-      @if (showButton) {
-          <button (click)="installPWA()">Установить PWA</button>
-      }
+    @if (showButton) {
+      <button (click)="installPWA()">
+        {{ 'pwa.install' | translate }}
+      </button>
+    }
   `,
   styles: [`
     lg-pwa-install {
@@ -14,18 +17,27 @@ import {Component, OnInit} from '@angular/core';
     }
 
     button {
-      background-color: #007bff;
+      background-image: linear-gradient(45deg, #3F51B5, #9C27B0);
       color: white;
       border: none;
       padding: 10px 20px;
       cursor: pointer;
-      width: 100%;
+      position: fixed;
+      bottom: 15px;
+      box-shadow: -15px 6px 8px 0px #eed2f0;
+      left: 0;
+      border-radius: 0 16px 16px 0;
+      background-size: calc(100% + 70px) 100%;
+      transition: background-position 0.3s ease;
     }
 
     button:hover {
-      background-color: #0056b3;
+      background-position: -70px 0;
     }
   `],
+  imports: [
+    TranslatePipe
+  ]
 })
 export class PwaInstallComponent implements OnInit {
   deferredPrompt: any = null;
