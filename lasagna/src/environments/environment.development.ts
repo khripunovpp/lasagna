058e993599b2d3b34c73156110292b81326e2d62
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-08-09 15:28:13 +0100

    Revert "Revert "versioning build""
    This reverts commit 15e4ae1e

--- lasagna/src/environments/environment.development.ts
+++ lasagna/src/environments/environment.development.ts
@@ -1,3 +1,7 @@
+declare const window: any;
+
 export const environment = {
   production: false,
+  // Версия будет автоматически заменена при билде
+  version: (typeof window !== 'undefined' && window.__VERSION__) || 'dev-build',
 };
