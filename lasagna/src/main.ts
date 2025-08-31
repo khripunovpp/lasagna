155658d9bdc113461d19bafa5ddb6c8d0d824c23
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-08-31 21:10:43 +0100

    enableProfiling

--- lasagna/src/main.ts
+++ lasagna/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import * as Sentry from '@sentry/angular';
+import { enableProfiling } from '@angular/core';
 
 Sentry.init({
   dsn: "https://6f5f68ff28550996e10e8c4a49edc46e@o4509209983057920.ingest.de.sentry.io/4509209987645520",
@@ -9,6 +10,8 @@ Sentry.init({
   // For example, automatic IP address collection on events
   sendDefaultPii: true
 });
+
+enableProfiling();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
