9be2c79a054220113860fd243acc322339f3a2bf
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-09-06 10:12:20 +0200

    title i18n

--- lasagna/src/app/features/setup-defaults/setup-defaults.initializer.ts
+++ lasagna/src/app/features/setup-defaults/setup-defaults.initializer.ts
@@ -2,15 +2,32 @@ import {inject} from '@angular/core';
 import {SetupDefaultsService} from './setup-defaults.service';
 import {NotificationsService} from '../../shared/service/services';
 import {errorHandler} from '../../shared/helpers';
+import {SettingsService} from '../settings/service/services/settings.service';
+import {Title} from '@angular/platform-browser';
+import {TranslateService} from '@ngx-translate/core';
 
-export const setupDefaultsInitializer = () => {
+export const setupDefaultsInitializer = async () => {
   const setupDefaultsService = inject(SetupDefaultsService);
   const notificationsService = inject(NotificationsService);
+  const settingsService = inject(SettingsService);
+  const title = inject(Title);
+  const translateService = inject(TranslateService);
 
-  return Promise.all([
-    setupDefaultsService.setupRecipesCategories(),
-    setupDefaultsService.setupProductsCategories(),
-    setupDefaultsService.setupProducts(),
-    setupDefaultsService.setUserUUID(),
-  ]).catch(error => notificationsService.error(errorHandler(error)));
+  try {
+    return await Promise.all([
+      setupDefaultsService.setupRecipesCategories(),
+      setupDefaultsService.setupProductsCategories(),
+      setupDefaultsService.setupProducts(),
+      setupDefaultsService.setUserUUID(),
+      settingsService.loadSettings()
+        .then((settings) => settingsService.setDefaultSettings())
+        .then(async (settings) => {
+          const lang = settings?.getSetting<string>('lang')?.data || 'en';
+          await settingsService.changeLang(lang);
+          title.setTitle(translateService.instant('app.title'));
+        }),
+    ]);
+  } catch (error) {
+    return notificationsService.error(errorHandler(error));
+  }
 };
