3b541ad92b9e847c3b926c3f0f7935a1da1de7ec
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-09-27 17:18:22 +0100

    sub search for products

--- lasagna/src/app/features/global-search/global-search.component.ts
+++ lasagna/src/app/features/global-search/global-search.component.ts
@@ -1,13 +1,12 @@
-import {ChangeDetectionStrategy, Component, computed, effect, HostListener, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, effect, HostListener, inject, signal} from '@angular/core';
 import {InputComponent} from '../controls/form/input.component';
-import {GlobalSearchService, SearchResultContext} from './global-search.service';
+import {AdditionalData, GlobalSearchService, SearchResultContext} from './global-search.service';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
-import {combineLatestWith, debounceTime, from, Observable, of, startWith, switchMap, tap} from 'rxjs';
-import {AsyncPipe, NgTemplateOutlet} from '@angular/common';
+import {combineLatestWith, debounceTime, defer, from, Observable, of, startWith, switchMap, tap} from 'rxjs';
+import {AsyncPipe, JsonPipe, NgTemplateOutlet} from '@angular/common';
 import {FadeInComponent} from '../../shared/view/ui/fade-in.component';
 import {FocusTrapDirective} from '../../shared/view/directives/focus-trap.directive';
-import {groupBy} from '../../shared/helpers/grouping.helper';
 import {TitleComponent} from '../../shared/view/layout/title.component';
 import {TranslatePipe} from '@ngx-translate/core';
 import {TimeAgoPipe} from '../../shared/view/pipes/time-ago.pipe';
@@ -15,6 +14,7 @@ import {BODY_LOCKER} from '../../shared/service/providers/body-locker.provider';
 import {FlexColumnComponent} from '../../shared/view/layout/flex-column.component';
 import {injectQueryParams} from '../../shared/helpers';
 import {toObservable} from '@angular/core/rxjs-interop';
+import {ExpanderComponent} from '../../shared/view/ui/expander.component';
 
 @Component({
   selector: 'lg-global-search',
@@ -32,7 +32,8 @@ import {toObservable} from '@angular/core/rxjs-interop';
                         [formControl]="searchControl"></lg-input>
             </div>
 
-            @let results = results$|async;
+            @let results = results$ | async;
+            @let additionalData = additionalData$ | async;
 
             @if (results?.length) {
               <div #resultsDomRef class="lg-global-search__results">
@@ -42,7 +43,8 @@ import {toObservable} from '@angular/core/rxjs-interop';
                                  [ngTemplateOutletContext]="{
                                      $implicit: item.result,
                                      caption: 'search.product.title' | translate,
-                                     itemTpl: productItemTpl
+                                     itemTpl: productItemTpl,
+                                     subTpl: productItemSubTpl,
                                    }">
                     </ng-template>
 
@@ -54,7 +56,29 @@ import {toObservable} from '@angular/core/rxjs-interop';
                         }
                       </a>
                     </ng-template>
+
+                    <ng-template #productItemSubTpl let-searchData>
+                      @let additionalResults = additionalData?.[item.context]?.[searchData?.uuid || '']?.data || [];
+                      @if (additionalResults?.length) {
+                        <lg-expander [flat]="true"
+                                     [once]="true"
+                                     [openLabel]="'Found ' + (additionalResults?.length ?? 0) + ' more with same source. Click to expand.'">
+
+                          <lg-flex-column [size]="'tiny'">
+                            @for (res of additionalResults; track res.uuid) {
+                              <ng-container *ngTemplateOutlet="recipeItemTpl; context: {$implicit: res}"></ng-container>
+                            }
+                          </lg-flex-column>
+                        </lg-expander>
+                      }
+                    </ng-template>
                   }
+
+                  <ng-template #recipeItemTpl let-data>
+                    <a [routerLink]="['/recipes/edit/', data?.uuid]">
+                      {{ data?.name }}
+                    </a>
+                  </ng-template>
 
                   @if (item.context === 'recipe') {
                     <ng-template [ngTemplateOutlet]="sectionTpl"
@@ -63,12 +87,6 @@ import {toObservable} from '@angular/core/rxjs-interop';
                                      caption: 'search.recipes.title' | translate,
                                      itemTpl: recipeItemTpl
                                    }">
-                    </ng-template>
-
-                    <ng-template #recipeItemTpl let-data>
-                      <a [routerLink]="['/recipes/edit/', data?.uuid]">
-                        {{ data?.name }}
-                      </a>
                     </ng-template>
                   }
 
@@ -151,7 +169,7 @@ import {toObservable} from '@angular/core/rxjs-interop';
         </div>
       </section>
 
-      <ng-template #sectionTpl let-items let-caption="caption" let-itemTpl="itemTpl">
+      <ng-template #sectionTpl let-items let-caption="caption" let-itemTpl="itemTpl" let-subTpl="subTpl">
         <lg-fade-in>
           <lg-flex-column size="medium">
             <div class="lg-global-search__results-caption">
@@ -163,7 +181,15 @@ import {toObservable} from '@angular/core/rxjs-interop';
                 <div class="lg-global-search__item">
                   <ng-container *ngTemplateOutlet="itemTpl; context: {$implicit: res.data}"></ng-container>
 
-                  {{ 'edited-at-label'|translate }} {{ (res.data?.updatedAt || res.data?.createdAt) | timeAgo }}
+                  <div class="lg-global-search__item-date text-muted text-cursive">
+                    {{ 'edited-at-label'|translate }} {{ (res.data?.updatedAt || res.data?.createdAt) | timeAgo }}
+                  </div>
+
+                  @if (subTpl) {
+                    <div class="lg-global-search__item-footer">
+                      <ng-container *ngTemplateOutlet="subTpl; context: {$implicit: res.data}"></ng-container>
+                    </div>
+                  }
                 </div>
               }
             </lg-flex-column>
@@ -218,11 +244,12 @@ import {toObservable} from '@angular/core/rxjs-interop';
       gap: 32px;
       max-height: 400px;
 
-    .lg-global-search__language-badge {
-      font-size: 0.8em;
-      color: var(--text-muted);
-      font-style: italic;
-    }
+      .lg-global-search__language-badge {
+        font-size: 0.8em;
+        color: var(--text-muted);
+        font-style: italic;
+      }
+
       overflow-y: auto;
       background-color: rgba(255, 255, 255, 0.7);
       margin-top: 32px;
@@ -239,6 +266,23 @@ import {toObservable} from '@angular/core/rxjs-interop';
       flex-direction: column;
       gap: 8px;
     }
+
+    .lg-global-search__item {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 2px;
+
+      &-footer {
+        width: 100%;
+        font-size: 0.8em;
+        opacity: 0.7;
+      }
+
+      &-date {
+        margin-left: auto;
+        font-size: 0.7em;
+      }
+    }
   `],
   imports: [
     InputComponent,
@@ -252,7 +296,9 @@ import {toObservable} from '@angular/core/rxjs-interop';
     TranslatePipe,
     TimeAgoPipe,
     FlexColumnComponent,
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    JsonPipe,
+    ExpanderComponent
   ]
 })
 export class GlobalSearchComponent {
@@ -261,6 +307,7 @@ export class GlobalSearchComponent {
   ) {
   }
 
+  readonly additionalData$: Observable<AdditionalData> = defer(() => this._globalSearchService.additionalDataSubject.asObservable());
   readonly showBar = computed(() => this._globalSearchService.displayBar());
   readonly searchQueryParams = injectQueryParams('search');
   readonly searchControl = new FormControl<string>(''); // Используем signal для хранения состояния поиска
@@ -299,23 +346,15 @@ export class GlobalSearchComponent {
     switchMap(([value, query,]) => {
       const valSearch = value?.trim();
       const valQuery = query?.toString().trim();
-
-      if (valSearch) {
-        return from(this._globalSearchService.search(valSearch));
-      } else if (valQuery) {
-        return from(this._globalSearchService.search(valQuery));
+      const searchVal = valSearch || valQuery || '';
+      if (!searchVal) {
+        return of([]);
       }
-      return of([]);
+      return from(this._globalSearchService.search(searchVal));
     }),
 
-    switchMap((results) => {
-      const group = groupBy(results, 'context');
-      return of(Object.entries(group).map(([key, value]) => {
-        return {
-          context: key as SearchResultContext,
-          result: value,
-        } as any;
-      }));
+    tap(results => {
+      this._globalSearchService.runSecondSearch(results);
     }),
   );
   readonly #_bodyLocker = inject(BODY_LOCKER);
