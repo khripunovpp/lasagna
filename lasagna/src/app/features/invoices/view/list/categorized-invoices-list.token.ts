5e4d49876687c47119cde1b1b51c913c49ec0ae1
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2026-04-19 12:39:25 +0100

    [client] fix invoices error

--- lasagna/src/app/features/invoices/view/list/categorized-invoices-list.token.ts
+++ lasagna/src/app/features/invoices/view/list/categorized-invoices-list.token.ts
@@ -1,12 +1,19 @@
 import {inject, InjectionToken} from '@angular/core';
-import {from, map, mergeMap, Observable, switchMap} from 'rxjs';
+import {from, map, mergeMap, Observable, of, switchMap} from 'rxjs';
 import {groupBy} from '../../../../shared/helpers/grouping.helper';
 import {InvoicesRepository} from '../../service/Invoices.repository';
 import {Invoice} from '../../service/Inovice/Invoice';
+import {HAS_FEATURE} from '../../../settings/service/providers/has-feature.token';
 
 export const CATEGORIZED_INVOICES_LIST = new InjectionToken<Observable<any>>('CategorizedInvoicesList', {
   factory: () => {
     const invoicesRepository = inject(InvoicesRepository);
+    const isDevMode = inject(HAS_FEATURE)('invoices');
+
+    if (isDevMode) {
+      return of([]);
+    }
+
     const invoices = from(invoicesRepository.loadToObservable()).pipe(
       switchMap(() => invoicesRepository.items$),
     );
@@ -15,27 +22,26 @@ export const CATEGORIZED_INVOICES_LIST = new InjectionToken<Observable<any>>('Ca
       map((invoices: Invoice[]) => invoices.toSorted((a: Invoice, b: Invoice) => a.prefix.localeCompare(b.prefix))),
       map((invoices: Invoice[]) => groupBy(invoices, 'prefix')),
       mergeMap(async (grouped: Record<string, Invoice[]>) => {
-          const list = [];
+        const list = [];
 
-          for (const groupedKey in grouped) {
-            list.push({
-              group_key: groupedKey,
-              items: grouped[groupedKey]?.toSorted((a, b) => a.createdAt > b.createdAt ? -1 : 1) || [],
-            });
-          }
-
-          if (!list.length) return [];
-
-          const [first, ...sortedList] = list.toSorted((a, b) => a.group_key > b.group_key ? 1 : -1);
-
-          // без категории всегда внизу
-          if (first?.group_key) {
-            return [first].concat(sortedList);
-          }
-
-          return sortedList.concat([first]);
+        for (const groupedKey in grouped) {
+          list.push({
+            group_key: groupedKey,
+            items: grouped[groupedKey]?.toSorted((a, b) => a.createdAt > b.createdAt ? -1 : 1) || [],
+          });
         }
-      ),
+
+        if (!list.length) return [];
+
+        const [first, ...sortedList] = list.toSorted((a, b) => a.group_key > b.group_key ? 1 : -1);
+
+        // без категории всегда внизу
+        if (first?.group_key) {
+          return [first].concat(sortedList);
+        }
+
+        return sortedList.concat([first]);
+      }),
     );
   }
 })
